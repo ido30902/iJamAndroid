0f04a8e6a391baeb1b341e2a8b83f1deacec40f9
ido30902 <idobarabi@gmail.com>
2020-06-11 21:25:34 +0300

    Changed URL

--- app/src/main/java/com/example/ijamapp/Utilities/NetworkProperties.java
+++ app/src/main/java/com/example/ijamapp/Utilities/NetworkProperties.java
@@ -2,6 +2,6 @@ package com.example.ijamapp.Utilities;
 
 public class NetworkProperties
 {
-    public final static String SERVER_MANAGER_URL = "http://13.59.2.26/API/servermanager.php"; //http://13.59.2.26/API/servermanager.php
+    public final static String SERVER_MANAGER_URL = "http://www.ijam.live/API/servermanager.php";
 }
 
